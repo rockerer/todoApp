9f334f1049d9a4339915fa8235cb7f6d9fa87902
Florian Aul <florian@em.cs.uni-frankfurt.de>
2021-01-02 00:06:17 +0100

    checking for negative index

--- todo.cpp
+++ todo.cpp
@@ -23,14 +23,14 @@ void Todo::insertTodo(TodoEntry &&todoEntry){
 }
 
 std::optional<TodoEntry> Todo::getTodo(int index){
-    if(index < static_cast<int>(this->m_vec_todo.size())) {
+    if(index >= 0 && index < static_cast<int>(this->m_vec_todo.size())) {
         return std::optional(this->m_vec_todo.at(index));
     }
     return std::nullopt;
 }
 
 void Todo::updateTodo(int index, TodoEntry &&newTodoEntry){
-    if(index < static_cast<int>(this->m_vec_todo.size())) {
+    if(index >= 0 && index < static_cast<int>(this->m_vec_todo.size())) {
         this->deleteTodo(index);
         this->m_vec_todo.emplace(
                this->m_vec_todo.begin() + index, std::forward<TodoEntry>(newTodoEntry));
@@ -40,7 +40,7 @@ void Todo::updateTodo(int index, TodoEntry &&newTodoEntry){
     }
 }
 void Todo::deleteTodo(int index){
-    if(index < static_cast<int>(this->m_vec_todo.size())) {
+    if(index >= 0 && index < static_cast<int>(this->m_vec_todo.size())) {
         this->m_vec_todo.erase(
                 this->m_vec_todo.begin() +  index);
     }
